ad10caec3257a58dab8cd1f14895cc789bb93592
PJDEVEX <piyankara.jayadewa@gmail.com>
2024-03-28 19:54:20 +0100

    feat: load docs

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='chromadb',
-    version='0.0.1',
-    author='piyankara Jayadewa (PJ)',
-    author_email='piyankara.jayadewa@gmail.com',
-    install_requires=["openai","langchain","python-dotenv","tiktoken", "chromadb"],
+    name="chromadb",
+    version="0.0.1",
+    author="piyankara Jayadewa (PJ)",
+    author_email="piyankara.jayadewa@gmail.com",
+    install_requires=[
+        "openai",
+        "langchain",
+        "python-dotenv",
+        "tiktoken",
+        "chromadb",
+        "langchain-openai",
+        "langchain_community",
+        "unstructured",
+        "black",
+    ],
     packages=find_packages(),
 )
